dacce63b8bf92a7435e5e2fe7a922b5216df3b00
LASHA KATCHEISHVILI <katcheishvili.l@gmail.com>
2021-09-19 15:14:49 +0400

    add-nuget

---
+++ Common/ServiceInjection/ServiceInjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.ServiceInjection
+{
+    public static class ServiceInjectionExtensions
+    {
+        public static IServiceCollection AddJobariaService(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
