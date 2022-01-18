6cd7784ff39e2c755be2b8a62b0836eda27172ad
LASHA KATCHEISHVILI <katcheishvili.l@gmail.com>
2022-01-18 11:10:26 +0400

    add-table

---
+++ Domain/Migrations/20211201072616_asd_dlslsl.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domain.Migrations
+{
+    public partial class asd_dlslsl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "UserAnimals",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "UserAnimals");
+        }
+    }
+}
